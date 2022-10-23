2f673e9f306b4eb0df943472dc2843a176af40b2
gabrielSanovis <9novis9@gmail.com>
2022-10-23 20:41:47 -0300

    Create Component CardBillings

---
+++ src/components/CardBillings.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { CurrencyCircleDollar, FilePdf } from "phosphor-react";
+import { ICardBillingsProps } from "./CardBillings";
+import { CardBillings } from "./CardBillings";
+import { InfoStatus } from "./InfoStatus";
+
+export default {
+  title: "Components/CardBillings",
+  component: CardBillings.Root,
+  args: {
+    plan: "plano plus",
+    infoStatus: <InfoStatus label="pendente" color="golden" />,
+    date: "20 de Nov de 2022",
+    coin: "R$",
+    price: "200,00",
+    children: (
+      <CardBillings.Button>
+        <CardBillings.Icon>
+          <CurrencyCircleDollar weight="fill" />
+        </CardBillings.Icon>
+        <CardBillings.Icon>
+          <FilePdf weight="fill" />
+        </CardBillings.Icon>
+      </CardBillings.Button>
+    ),
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+    infoStatus: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta<ICardBillingsProps>;
+
+export const Default: StoryObj<ICardBillingsProps> = {};
