2aa33f731dbe0cadfecfe88123ae2e91baf5a8b0
Leonardo Amaro <la95112@gmail.com>
2022-12-21 21:09:38 -0300

    Adicionei um componente novo Searcheable Select

--- src/components/Device.stories.tsx
+++ src/components/Device.stories.tsx
@@ -10,15 +10,7 @@ export default {
     number: "+55 (81) 91234-5678",
     nameDevice: "Nome do Device",
     image: "https://cdn-icons-png.flaticon.com/512/2504/2504845.png",
-  },
-  argTypes: {
-    status: {
-      options: ["on", "off"],
-      control: {
-        type: "inline-radio",
-      },
-    },
-  },
+  }
 } as Meta<IDeviceProps>;
 
 export const Default = {};
