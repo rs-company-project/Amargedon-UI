2ffdb5f5d70d8fe4aa7abf858b7f1a59853b99fd
Leonardo Amaro <58569618+leonardoamaro-dev@users.noreply.github.com>
2022-12-21 21:14:07 -0300

    Update Device.stories.tsx

--- src/components/Device.stories.tsx
+++ src/components/Device.stories.tsx
@@ -10,6 +10,14 @@ export default {
     number: "+55 (81) 91234-5678",
     nameDevice: "Nome do Device",
     image: "https://cdn-icons-png.flaticon.com/512/2504/2504845.png",
+  },
+   argTypes: {
+    status: {
+      options: ["on", "off"],
+      control: {
+        type: "inline-radio",
+      },
+    },
   }
 } as Meta<IDeviceProps>;
 
